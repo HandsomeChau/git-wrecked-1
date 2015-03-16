d6ad91540107ea58a1b147ea1595b5bbfc5ae9b7
Edwin Pham <edwin.pham@yahoo.com>
2015-03-16 10:42:29 -0700

    Present user with success registration. Insert user information into table

--- application/views/form_success.php
+++ application/views/form_success.php
@@ -1,7 +1,6 @@
 <div class="jumbotron" id="jumbotron">
     <div id="jumbotron_inner">
         <h1>Thank you!</h1>
-
         <p>
             You can now log in. Thank you for choosing us.
         </p>
